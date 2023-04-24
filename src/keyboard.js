9628dfe169718f39975e6649fa658bcea8c62dcb
AleksaRoad <aleksaroad@gmail.com>
2023-04-24 19:46:42 +0300

    feat: css for keyboard and create keys, change favicon

--- src/keyboard.js
+++ src/keyboard.js
@@ -1,26 +1,52 @@
 import "./sass/index.scss";
 import { languageEn, languageRu } from "./language";
 
-function createTemplate(template, classname, text) {
+function createTemplate(template, classname, text, classname2) {
   const element = document.createElement(template);
-  element.className = classname;
+  element.className = classname2;
+  element.classList.add(classname);
   element.innerHTML = text;
   return element;
 }
-const wrapper = createTemplate("div", "wrapper", "");
+const wrapper = createTemplate("div", "wrapper", "", "wrapper");
 
 document.body.appendChild(wrapper);
-wrapper.appendChild(createTemplate("h1", "title", "RSS Virtual Keyboard"));
+wrapper.appendChild(
+  createTemplate("h1", "title", "RSS Virtual Keyboard", "title")
+);
+wrapper.appendChild(createTemplate("textarea", "textarea", "", "textarea"));
+
+const keyboard = createTemplate("div", "keyboard", "", "keyboard");
+wrapper.appendChild(keyboard);
 
 function createButton(language) {
   const array = Object.values(language);
-  // console.log(array);
   array.map((word) =>
-    wrapper.appendChild(createTemplate("button", "word-en", word))
+    keyboard.appendChild(
+      createTemplate("button", `${word}`.toLowerCase(), word, "word")
+    )
   );
+  console.log(array);
 }
-
+const word = document.getElementsByClassName("word");
+console.log(word);
 createButton(languageEn);
-createButton(languageRu);
 
-// console.log(languageEn, languageRu);
+wrapper.appendChild(
+  createTemplate(
+    "h3",
+    "text",
+    "Клавиатура создана на операционной системе Windows",
+    "text"
+  )
+);
+wrapper.appendChild(
+  createTemplate(
+    "h3",
+    "text",
+    "Для переключения языка комбинация: Shift + Alt",
+    "text"
+  )
+);
+
+console.log(languageEn, languageRu);
